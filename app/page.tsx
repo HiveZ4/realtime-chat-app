8f40bf9da9249b3820f9870dd1b7794655f7ed2e
CodeCompleteYT <techstack.yt@gmail.com>
2024-03-24 17:35:58 +1100

    4.3 Integrate Convex with Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button>Click Here</Button>;
+  return <UserButton />;
 }
